a855a740af9ea7a63bff91f583fbf663289cf1c9
Mohammed <mohammednaamiweb@gmail.com>
2025-03-16 03:26:39 +0100

    fix meta

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,12 +1,21 @@
+import { Metadata } from 'next';
 import DashboardProvider from './provider';
 import { Toaster } from "@/components/ui/sonner"
 
+
+export const metadata: Metadata = {
+    title: "Screenshot To Code",
+    description:
+      "Convert any screenshot or design to clean code",
+  };
 
 async function DashboardLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+
+
 
     return (
         <DashboardProvider>
